06a4b15d88d5c15edc5ba8cebcfe825f0cb4ac4a
SkyZH <iskyzh@gmail.com>
2018-07-05 14:23:08 +0800

    use join to merge dataframes

--- main.py
+++ main.py
@@ -24,32 +24,35 @@ async def main():
 
 async def transaction(ori_df, __index, __code, __multiplier):
     # In[1]: 获取所有期货
-    available_future = pd.Series(process.read_realtime_data(__code).index)
-    CTP.sub_market_data(available_future)
+    available_futures = pd.Series(process.read_realtime_data(__code).index)
+    CTP.sub_market_data(available_futures)
     print('获取期货信息...')
-    await CTP.wait_data_available(available_future)
-    # In[2]: 选择买入的期货
-    future_df = pd.DataFrame(list(map(
-        lambda data: (data.InstrumentID.decode('utf-8'), data.LastPrice, data.Volume, data.OpenInterest, data.UpdateTime),
-        [CTP.get_market_data(code) for code in available_future])
-    ))
-    future_df.columns = ['code', 'lastPrice', 'volume', 'openInterest', 'updateTime']
-    future_df = future_df.set_index('code')
-    future_df['total'] = future_df['openInterest'] * future_df['volume']
-    _id = future_df['total'].idxmax()
+    await CTP.wait_data_available(available_futures)
 
-    future = future_df.loc[_id]
+    # In[2]: 选择买入的期货
+    df_futures = pd.DataFrame(list(map(
+        lambda data: (data.InstrumentID.decode('utf-8'), data.LastPrice, data.Volume, data.OpenInterest, data.UpdateTime),
+        [CTP.get_market_data(code) for code in available_futures])
+    ), columns=['code', 'lastPrice', 'volume', 'openInterest', 'updateTime'])
+    df_futures = df_futures.set_index('code')
+    df_futures['total'] = df_futures['openInterest'] * df_futures['volume']
+    _id = df_futures['total'].idxmax()
+    future = df_futures.loc[_id]
     current_price = future['lastPrice'] * __multiplier
+
     # In[3]: 获取现货信息
     indexes = __index.set_index('code')
     print('获取现货信息...')
-    index_df = ts.get_realtime_quotes(pd.Series(indexes.index))[['code', 'price']]
+    df_indexes = ts.get_realtime_quotes(pd.Series(indexes.index))[['code', 'price']]
+    df_indexes['price'] = df_indexes['price'].astype(np.float64)
+    df_indexes = df_indexes.set_index('code')
+
     # In[4]: 计算市值
-    for code, price in index_df.values:
-        ori_df.loc[code, 'price'] = price
-    ori_df['price'] = ori_df['price'].astype(np.float64)
-    ori_df['value'] = ori_df['price'] * ori_df['position']
-    total = ori_df['value'].sum()
+    df_data = ori_df.copy()
+    df_data = df_data.join(df_indexes)
+    df_data['value'] = df_data['price'] * df_data['position']
+    total = df_data['value'].sum()
+
     # In[5]: 计算合约数
     return _id, total, current_price, total / current_price
 
